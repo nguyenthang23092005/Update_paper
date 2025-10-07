9b57819f4c338d7f6b98e30d015ae699fc54d0f5
nguyenthang23092005 <nguyenvanthang230925@gmail.com>
2025-10-07 14:21:39 +0700

    Update

--- scholar_search.py
+++ scholar_search.py
@@ -9,7 +9,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
-
+from selenium.webdriver.chrome.service import Service
 
 def get_target_date(days_ago=1):
     """Lấy ngày YYYY-MM-DD của hôm qua (hoặc n ngày trước)"""
@@ -25,18 +25,16 @@ class ScholarFinder:
         options.add_argument("--headless=new")
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
-        options.add_argument("--disable-gpu")
-        options.add_argument("--disable-extensions")
-        options.add_argument("--disable-software-rasterizer")
-        options.add_argument("--remote-debugging-port=9222")
-        options.add_argument("--window-size=1920,1080")
         options.add_argument('--disable-blink-features=AutomationControlled')
         options.add_experimental_option("excludeSwitches", ["enable-automation"])
         options.add_experimental_option('useAutomationExtension', False)
+        options.add_argument("--user-data-dir=/tmp/chrome-profile")
+        
+        self.service = Service("/usr/bin/chromedriver")
 
-        driver = webdriver.Chrome(options=options)
-        driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
-        self.driver = driver
+        self.driver = webdriver.Chrome(options=options)
+        # self.driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+        self.driver.execute_cdp_cmd( "Page.addScriptToEvaluateOnNewDocument", {"source": "Object.defineProperty(navigator, 'webdriver', {get: () => undefined})"} )
         return self.driver
 
     def extract_pub_date(self, authors_text: str):
