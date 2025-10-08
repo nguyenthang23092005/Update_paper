4b4f77b5d4e9c97a84f2a6c811988990f2415c94
nguyenthang23092005 <nguyenvanthang230925@gmail.com>
2025-10-08 09:43:38 +0700

    Update

--- scholar_search.py
+++ scholar_search.py
@@ -29,6 +29,9 @@ class ScholarFinder:
         options.add_experimental_option("excludeSwitches", ["enable-automation"])
         options.add_experimental_option('useAutomationExtension', False)
         options.add_argument("--user-data-dir=/tmp/chrome-profile")
+        options.add_argument("window-size=1920,1080")
+        options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/141.0.0.0 Safari/537.36")
+
 
         self.driver = webdriver.Chrome(options=options)
         # self.driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
